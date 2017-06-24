407aa12857299d9a8ed0b381a45d3826fdcc3d25
lucivanbatista <lucivanbatista98@gmail.com>
2017-06-24 18:06:22 -0300

    Habilidades do personagem warrior de fogo

--- Assets/script/warrior.cs
+++ Assets/script/warrior.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class warrior : MonoBehaviour {
 
 	public GameObject Fireball;
+	public GameObject WallOfFire;
+	public GameObject MeteorSwarm;
 	int passos = 0;
 	int limitePassos;
 	int mana = 50;
@@ -15,30 +17,51 @@ public class warrior : MonoBehaviour {
 	}
 
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.LeftArrow)){
-			this.gameObject.GetComponent<Animation> ().Play("Walk");
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			this.gameObject.GetComponent<Animation> ().Play ("Walk");
 
-			this.gameObject.transform.Rotate (new Vector3(0.0f, -90f, 0.0f));
-		}else if (Input.GetKeyDown(KeyCode.RightArrow)){
-			this.gameObject.GetComponent<Animation> ().Play("Walk");
+			this.gameObject.transform.Rotate (new Vector3 (0.0f, -90.0f, 0.0f));
+		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			this.gameObject.GetComponent<Animation> ().Play ("Walk");
 
-			this.gameObject.transform.Rotate (new Vector3(0.0f, 90f, 0.0f));
-		}else if (Input.GetKeyDown(KeyCode.UpArrow)){
-			this.gameObject.GetComponent<Animation> ().Play("Walk");
-			this.gameObject.transform.Translate(new Vector3(0.0f, 0.0f, 2.5f));
+			this.gameObject.transform.Rotate (new Vector3 (0.0f, 90.0f, 0.0f));
+		} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			this.gameObject.GetComponent<Animation> ().Play ("Walk");
+			this.gameObject.transform.Translate (new Vector3 (0.0f, 0.0f, 2.5f));
 			passos++;
-		}else if (Input.GetKeyDown(KeyCode.DownArrow)){
-			this.gameObject.GetComponent<Animation> ().Play("Walk");
-			this.gameObject.transform.Translate(new Vector3(0.0f, 0.0f, -2.5f));
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			this.gameObject.GetComponent<Animation> ().Play ("Walk");
+			this.gameObject.transform.Translate (new Vector3 (0.0f, 0.0f, -2.5f));
 			passos++;
-		}else if(Input.GetKeyDown(KeyCode.Space)){
-			this.gameObject.GetComponent<Animation> ().Play("Attack");
+		} else if (Input.GetKeyDown (KeyCode.Alpha1)) { //Habilidade 1
+			this.gameObject.GetComponent<Animation> ().Play ("Attack");
 			GameObject fb = Instantiate (Fireball) as GameObject;
 			fb.transform.position = this.gameObject.transform.position + (this.gameObject.transform.forward);
 			fb.gameObject.transform.Translate (new Vector3 (-0.5f, 1.0f, 0.0f));
 			fb.transform.rotation = this.gameObject.transform.rotation;
-			fb.GetComponent<Rigidbody>().AddForce(Fireball.transform.forward * 500.0f);
+			fb.GetComponent<Rigidbody> ().AddForce (Fireball.transform.forward * 500.0f);
+			/*if(fb.gameObject.transform.position.x >= this.gameObject.transform.position.x + (4.0f * 1.25f) || 
+				fb.gameObject.transform.position.z >= this.gameObject.transform.position.z + (4.0f * 1.25f)){
+				Debug.Log ("ENTROU");
+				Destroy (fb);
+			}*/
+		} else if (Input.GetKeyDown (KeyCode.Alpha2)) { //Habilidade 2
+			this.gameObject.GetComponent<Animation> ().Play ("Attack");
+			GameObject fb = Instantiate (WallOfFire) as GameObject;
+			fb.transform.position = this.gameObject.transform.position + (this.gameObject.transform.forward);
+			fb.gameObject.transform.Translate (new Vector3 (-1.25f, 0.05f, 2.0f));
+			fb.transform.rotation = this.gameObject.transform.rotation;
+		} else if (Input.GetKeyDown (KeyCode.Alpha3)) { //Habilidade 3
+			this.gameObject.GetComponent<Animation> ().Play ("Attack");
+			GameObject fb = Instantiate (MeteorSwarm) as GameObject;
+			fb.transform.position = this.gameObject.transform.position + (this.gameObject.transform.forward);
+			fb.gameObject.transform.Translate (new Vector3 (-1.25f, 0.05f, 2.0f));
+			fb.transform.rotation = this.gameObject.transform.rotation;
 		}
+
+
+
+		//Comando para limitar o plano (Não sair do plano)
 		Vector3 pos = this.gameObject.transform.position;
 		this.gameObject.transform.position = new Vector3(Mathf.Clamp(pos.x, 1.25f , 48.75f), pos.y, Mathf.Clamp(pos.z, 1.25f, 48.75f));
 	}
